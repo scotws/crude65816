ebce16dc4bf6d91c8fa3ecf5696de309fb566969
Scot W. Stevenson <scot.stevenson@gmail.com>
2016-12-24 01:25:59 +0100

    Added test program for Mock Mensch Monitor routines, tested PUT_CHR, PUT_STR, SEND_CR

--- config.fs
+++ config.fs
@@ -1,6 +1,6 @@
 \ Configuration file for A Crude 65816 Emulator
 \ Scot W. Stevenson <scot.stevenson@gmail.com>
-\ This version: 22. Dec 2016
+\ This version: 23. Dec 2016
 
 \ This file must be included in crude65816.fs before io.fs
 
@@ -12,7 +12,7 @@
 \ 00e000 s" roms/rom65816.bin" loadrom  \ operating system, BIOS, *DEFAULT*
 \ 00e000 s" roms/rom65c02.bin" loadrom  \ test program for the 65c02
 
-\ 00e000 s" roms/mmm/mmm.bin" loadrom   \ Mock Mensch Monitor W65C265SXB ROM
+00e000 s" roms/mmm/mmm.bin" loadrom   \ Mock Mensch Monitor W65C265SXB ROM
 
 \ Dummy file to show how other ROM data is loaded. Later, these can be program
 \ libraries or other ROM chips.
@@ -20,7 +20,7 @@
 
 \ Tests. Unless your name is Scot, you probably don't want to touch these
 \ 008000 s" tests/tink.bin" loadrom
-\ 008000 s" tests/test_mmm.bin" loadrom
+008000 s" roms/mmm/test_mmm.bin" loadrom
 
 \  --- I/O ADRESSES --- 
 \  These are referenced by io.fs
@@ -31,14 +31,14 @@
 
 \ ** Default I/O addresses ** 
 \ Use these when you don't care
-00ff00 value putchr
-00ff01 value getchr   \ blocks until character received
+\ 00ff00 value putchr
+\ 00ff01 value getchr   \ blocks until character received
 
 \ ** Addresses for the W65C265SXB MMM ** 
 \ These are for use with the Mock Mensch Monitor emulation ROM for the 265SXB.
 \ We're so crude in this version that we just need an address as a hook,
 \ regardless if it is where the real 265SXB writes to or not. These are subject
 \ to change as the emulation gets better.
-\ 00df75 value getchr   \ actually the data register of UART 2
-\ 00df77 value putchr   \ actually the data register of UART 3 
+00df75 value getchr   \ actually the data register of UART 2
+00df77 value putchr   \ actually the data register of UART 3 
 
